d3c2d8fd3fe8ec2d59bfb5db55a8ef790f05744c
mikeyyy12 <shubhamkarmyal95@gmail.com>
2026-01-21 21:36:47 +0530

    server: add ws and get rtpCapabilities

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,35 @@
 import express from "express";
 import {initRouter, initWebRtcServer} from "./worker.js"
-const app = express();
-const PORT = 5000;
+import { WebSocketServer } from "ws";
 
 
 async function start() {
 
+  const server = new WebSocketServer({ port: 8080 });
+    const router = await initRouter();
+  server.on('connection', socket => {
+  console.log('Client connected');
+    console.log(router.rtpCapabilities);
+  socket.on('message', message => {
+    const data = JSON.parse(message.toString());
+    switch(data.type){
+      case "rtpCapabilities":
+        socket.send(JSON.stringify({
+          type:"rtpCapabilities",
+          rtpCapabilities:router.rtpCapabilities
+        }))
 
-  const router = await initRouter();
+    }
+    console.log(`Received: ${message}`);
+    // Echo the message back
+    socket.send(`Server says: ${message}`);
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
   const webRtcServer =await initWebRtcServer();
   console.log(router.id);
   const producer = await router.createWebRtcTransport(
