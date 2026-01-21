3b94fc4f024d383fea288f62f314ce10f35d4442
mikeyyy12 <shubhamkarmyal95@gmail.com>
2026-01-21 22:12:24 +0530

    frontend:added device and connected ws with backend to get rtpCap...

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,45 +1,68 @@
 "use client"
 import * as mediasoupClient from 'mediasoup-client';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
+  const deviceRef = useRef<mediasoupClient.Device | null>(null);
+  const [loaded, setLoaded] = useState(false);
+
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080');
+    const ws = new WebSocket('ws://localhost:8081');
+    socketRef.current = ws;
+    const newDevice = new mediasoupClient.Device();
+    const device = new mediasoupClient.Device();
+    deviceRef.current = device;
 
     ws.onopen = () => {
       console.log('WebSocket connected');
+      ws.send(JSON.stringify({ type: 'getRtpCapabilities' }));
     };
 
-    ws.onmessage = (event) => {
-      console.log('Message:', event.data);
+    ws.onmessage =async (event) => {
+
+      const data = JSON.parse(event.data)
+
+      console.log("data", data)
+      if (data.type == "rtpCapabilities") {
+        try{
+
+            await device.load({ routerRtpCapabilities: data.rtpCapabilities })
+
+           setLoaded(true)
+           console.log("Device loaded:", device.handlerName);
+        } catch (err: any) {
+          if (err.name === "UnsupportedError") {
+            console.warn("Browser not supported");
+          } else {
+            console.error(err);
+          }
+        }
+
+      }
+
+      console.log("\n device loaded",device.handlerName)
     };
 
     ws.onerror = (err) => {
       console.error('WebSocket error', err);
     };
-    setSocket(ws)
+
     return () => {
       ws.close();
       console.log('WebSocket closed')
     }
   }, [])
-  let device: mediasoupClient.Device;
 
-  try {
-    device = new mediasoupClient.Device();
-  }
-  catch (error) {
-    if ((error as any).name === 'UnsupportedError')
-      console.warn('browser not supported');
-  }
 
 
   return <div className="font-medium p-2 text-xl text-green-500">
     <p>Show Logs</p>
     <button
       onClick={() => {
+          const device = deviceRef.current;
+          if (!device) return;
         console.log(device.handlerName)
         console.log(device.loaded)
       }}
