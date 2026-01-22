afd919b7117b7cf92ace1b4496e22d5b4d888f24
mikeyyy12 <shubhamkarmyal95@gmail.com>
2026-01-22 17:23:06 +0530

    server:created produce and connect event

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,15 @@ import { WebSocketServer } from "ws";
 async function start() {
   const server = new WebSocketServer({ port: 8081 });
   const router = await initRouter();
-  server.on("connection", (socket) => {
+  server.on("connection", async(socket) => {
     console.log("Client connected");
+      const transport = await router.createWebRtcTransport({
+            webRtcServer,
+            enableUdp: true,
+            enableTcp: true,
+            preferUdp: true,
+            enableSctp: true,
+          });
     socket.on("message", async (message) => {
       const data = JSON.parse(message.toString());
 
@@ -20,13 +27,7 @@ async function start() {
           );
           break;
         case "createTransport":
-          const transport = await router.createWebRtcTransport({
-            webRtcServer,
-            enableUdp: true,
-            enableTcp: true,
-            preferUdp: true,
-            enableSctp: true,
-          });
+
           socket.send(
             JSON.stringify({
               type: "transportCreated",
@@ -37,6 +38,21 @@ async function start() {
               sctpParameters: transport.sctpParameters,
             }),
           );
+          break;
+        case "transport-connect":
+
+          await transport.connect({
+            dtlsParameters: {
+              role: "server",
+              fingerprints: [
+                {
+                  algorithm: "sha-256",
+                  value:
+                    "E5:F5:CA:A7:2D:93:E6:16:AC:21:09:9F:23:51:62:8C:D0:66:E9:0C:22:54:2B:82:0C:DF:E0:C5:2C:7E:CD:53",
+                },
+              ],
+            },
+          });
       }
     });
 
