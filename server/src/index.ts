009eb03f558e85a51b1ad28809ca1ee3eac71f1a
mikeyyy12 <shubhamkarmyal95@gmail.com>
2026-01-22 16:42:39 +0530

    server:add transport

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,51 +1,59 @@
 import express from "express";
-import {initRouter, initWebRtcServer} from "./worker.js"
+import { initRouter, initWebRtcServer } from "./worker.js";
 import { WebSocketServer } from "ws";
 
-
 async function start() {
-
   const server = new WebSocketServer({ port: 8081 });
-    const router = await initRouter();
-  server.on('connection', socket => {
-  console.log('Client connected');
-    console.log(router.rtpCapabilities);
-  socket.on('message', message => {
-    const data = JSON.parse(message.toString());
+  const router = await initRouter();
+  server.on("connection", (socket) => {
+    console.log("Client connected");
+    socket.on("message", async (message) => {
+      const data = JSON.parse(message.toString());
 
-    switch(data.type){
-      case "getRtpCapabilities":
-        socket.send(JSON.stringify({
-          type:"rtpCapabilities",
-          rtpCapabilities:router.rtpCapabilities
-        }))
-        break;
-    }
-
-
-  });
-
-  socket.on('close', () => {
-    console.log('Client disconnected');
-  });
-});
-
-  const webRtcServer =await initWebRtcServer();
-  console.log(router.id);
-  const producer = await router.createWebRtcTransport(
-    {
-    webRtcServer : webRtcServer,
-    enableUdp    : true,
-    enableTcp    : false
+      switch (data.type) {
+        case "getRtpCapabilities":
+          socket.send(
+            JSON.stringify({
+              type: "rtpCapabilities",
+              rtpCapabilities: router.rtpCapabilities,
+            }),
+          );
+          break;
+        case "createTransport":
+          const transport = await router.createWebRtcTransport({
+            webRtcServer,
+            enableUdp: true,
+            enableTcp: true,
+            preferUdp: true,
+            enableSctp: true,
+          });
+          socket.send(
+            JSON.stringify({
+              type: "transportCreated",
+              id: transport.id,
+              iceParameters: transport.iceParameters,
+              iceCandidates: transport.iceCandidates,
+              dtlsParameters: transport.dtlsParameters,
+              sctpParameters: transport.sctpParameters,
+            }),
+          );
+      }
     });
-const consumer = await router.createWebRtcTransport(
-  {
-    webRtcServer : webRtcServer,
-    enableUdp    : true,
-    enableTcp    : false
+
+    socket.on("close", () => {
+      console.log("Client disconnected");
+    });
   });
 
+  const webRtcServer = await initWebRtcServer();
+  console.log(router.id);
+  const transport = await router.createWebRtcTransport({
+    webRtcServer,
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    enableSctp: true,
+  });
 }
-
 
 start();
